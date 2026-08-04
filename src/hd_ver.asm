a286143db52ba759d8df1f903496c9b2c32ee9de
Kurogami2134 <90238011+Kurogami2134@users.noreply.github.com>
2026-08-04 19:57:35 -0300

    Disabled default stick functionality in HD version.

--- src/hd_ver.asm
+++ src/hd_ver.asm
@@ -8,6 +8,28 @@ HOOK_ADD    equ 0x088E7394
 PATCH_ADD   equ 0x088E7370
 INPUT_HOOK  equ 0x088A4A08
 
+PATCH2      equ 0x088E77E8
+PATCH3      equ 0x088E6EA0
+PATCH4      equ 0x088E6E64
+
 .createfile "../bin/hd.bin", 0
 .include "main.asm"
+
+;  disable normal stick functionality
+
+.word 0
+.word PATCH2
+.word 4
+.word 0
+
+.word 0
+.word PATCH4
+.word 4
+.word 0
+
+.word 0
+.word PATCH4
+.word 4
+.word 0
+
 .close
